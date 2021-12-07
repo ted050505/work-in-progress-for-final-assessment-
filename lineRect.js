fe5a069460fc3ade8b26bfcba7b250cd69639dc3
Jung Jun Hyeok <83487028+ted050505@users.noreply.github.com>
2021-12-07 15:32:00 +0900

    Creating an object tied to a line.

---
+++ lineRect.js
@@ -0,0 +1,40 @@
+class lineRect {
+  constructor(posX, posY, w) {
+    this.pos = createVector(posX, posY);
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
+    this.w = w;
+    
+    this.mass = random(0.8,1.5);
+  }
+  
+  applyForce(force) {
+    force.div(this.mass);
+    this.acc.add(force);
+  }
+  
+  chkEdge() {
+    if(this.pos.y >= height - this.w) {
+      this.vel.y *= -1/2;
+      // this.vel.set(0,0);
+      this.pos.y = height - this.w;
+    }
+    
+    if(this.pos.y <= height/2) {
+      this.vel.y *= -1/2;
+      this.pos.y = height/2;
+    }
+  }
+  
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    
+    this.acc.set(0, 0);
+  }
+  
+  display() {
+    line(this.pos.x + this.w/2, this.pos.y, this.pos.x + this.w/2, height);
+    rect(this.pos.x, this.pos.y, this.w, this.w);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,52 +1,40 @@
-let world;
-
 let myrect = [];
+let linerect = [];
 let slider;
 
-let boundaries = [];
-let boxes = [];
-
 function setup() {
-  createCanvas(600, 600);
-  
-  world = createWorld();
+  createCanvas(500, 700);
   
   for(let i=0; i<10; i++) {
     myrect[i] = new Rect(random(width), height/2, 50);
   }
+  for(let i=0; i<3; i++) {
+    linerect[i] = new lineRect(random(width), height/2, 50);
+  }
   slider = createSlider(0, 1, 0, 0);
   slider.position(10, 10);
-  
-  boundaries.push(new Boundary(width, height, width*2, 0));
-  for(let i=0;i<10;i++) {
-   let b = new Box(random(width), 100);
-   boxes.push(b);
-  }
 }
 
 function draw() {
-  let val = map(slider.value(), 0, 1, 0, 0.01);
+  let val = map(slider.value(), 0, 1, 0.1, -0.1);
   
   background(220);
   
-  let timeStep = val;
-  world.Step(timeStep, 30, 10);
-
+  let gravity = createVector(0, val);
   
-//   let gravity = createVector(0, val);
+  for(let i=0; i<10; i++) {
+    myrect[i].applyForce(gravity);
   
-//   for(let i=0; i<10; i++) {
-//     myrect[i].applyForce(gravity);
-//     myrect[i].chkEdge();
-//     myrect[i].update();
-//     myrect[i].display();
-//   }
-  
-
-  for (let i = 0; i < boundaries.length; i++) {
-    boundaries[i].display();
+    myrect[i].chkEdge();
+    myrect[i].update();
+    myrect[i].display();
   }
-  for (let i = boxes.length - 1; i >= 0; i--) {
-    boxes[i].display();
+  
+  for(let i=0; i<3; i++) {
+    linerect[i].applyForce(gravity);
+  
+    linerect[i].chkEdge();
+    linerect[i].update();
+    linerect[i].display();
   }
 }
