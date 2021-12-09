ebbe03292c18989748acb21580fa666d83908b78
Jung Jun Hyeok <ted0505@naver.com>
2021-12-10 05:16:53 +0900

    line height random

--- sketch.js
+++ sketch.js
@@ -6,14 +6,15 @@ let osc, playing, freq, amp;
 
 function setup() {
   createCanvas(500, 700);
-  
+
   osc = new p5.Oscillator('sine');
-  
+
   for(let i=0; i<10; i++) {
     myrect[i] = new Rect(random(width), height/2, 50);
   }
   for(let i=0; i<3; i++) {
     linerect[i] = new lineRect(random(width), height/2, 50);
+    linerect[i].set(random(5,10));
   }
   slider = createSlider(0, 1, 0, 0);
   slider.position(10, 10);
@@ -22,37 +23,37 @@ function setup() {
 function draw() {
   let val = map(slider.value(), 0, 1, 0.1, -0.1);
   let freq = map(slider.value(), 0, 1, 100, 400);
-  
+
   background(220);
-  
+
   let gravity = createVector(0, val);
-  
+
   for(let i=0; i<10; i++) {
     myrect[i].applyForce(gravity);
-  
+
     myrect[i].chkEdge();
     myrect[i].update();
     myrect[i].display();
   }
-  
+
   for(let i=0; i<3; i++) {
     linerect[i].applyForce(gravity);
-  
+
     linerect[i].chkEdge();
     linerect[i].update();
     linerect[i].display();
   }
-  
+
   if(freq < 201) {
     playOscillator();
     osc.amp(0.1, 0.1);
   }
-  
+
   if (playing) {
     osc.freq(freq, 0.1);
     osc.amp(amp, 0.1);
   }
-  
+
   print(freq);
 }
 
