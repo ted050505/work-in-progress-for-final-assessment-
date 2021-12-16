324bc88e7bb744ed841dda4767b3d13f492935a3
Jung Jun Hyeok <ted0505@naver.com>
2021-12-16 22:35:14 +0900

    Code ReFactoring

--- sketch.js
+++ sketch.js
@@ -7,32 +7,33 @@ let osc, playing, freq, amp;
 function setup() {
   createCanvas(1280, 780);
 
-  osc = new p5.Oscillator('sine');
-
   for(let i=0; i<10; i++) {
     myrect[i] = new Rect(random(width), height/2, 50);
   }
   for(let i=0; i<3; i++) {
     linerect[i] = new lineRect(random(width), height/2, 50);
   }
+
+  osc = new p5.Oscillator('sine');
+
   slider = createSlider(0, 1, 0, 0);
   slider.position(10, 10);
-  colorMode(HSB,50,50,20);
+
+  // colorMode(HSB,50,50,20);
 }
 
 function draw() {
   // 슬라이더 값 받기.
   let val = map(slider.value(), 0, 1, 0.1, -0.1);
-  let freq = map(slider.value(), 0, 1, 100, 400);
+  let freq = map(slider.value(), 0, 1, 200, 500);
 
-  background(100);
+  background(150);
 
   let gravity = createVector(0, val);
 
   // 상자들 제어.
   for(let i=0; i<10; i++) {
     myrect[i].applyForce(gravity);
-
     myrect[i].chkEdge();
     myrect[i].update();
     myrect[i].display();
@@ -41,22 +42,12 @@ function draw() {
   // 줄에 묶인 상자들 제어.
   for(let i=0; i<3; i++) {
     linerect[i].applyForce(gravity);
-
     linerect[i].chkEdge();
     linerect[i].update();
     linerect[i].display();
   }
 
-  if(freq < 201) {
-    playOscillator();
-  }
-
-  if(playing) {
-    osc.freq(freq, 0.3);
-    osc.amp(100, 0.1);
-  }else if(playing == false){
-    osc.amp(0, 0);
-  }
+  chkSliderValue();
 }
 
 function playOscillator() {
@@ -66,4 +57,16 @@ function playOscillator() {
 function mouseReleased() {
   osc.stop();
   playing = false;
+}
+
+function chkSliderValue() {
+  if(freq < 200) {
+    playOscillator();
+  }
+  if(playing) {
+    osc.freq(freq, 0.1);
+    osc.amp(1, 0.1);
+  }else if(playing == false){
+    osc.amp(0, 0);
+  }
 }
